353c975958b4017dbefb4efae0cbc763b17c5529
Karina <karina.shokur@devit-team.com>
2021-05-14 09:16:24 +0000

    removed testing code

--- src/app/dashboard/sponsor/sponsor.component.ts
+++ src/app/dashboard/sponsor/sponsor.component.ts
@@ -6,48 +6,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorComponent implements OnInit {
   constructor() { }
-  proposals = [
-    {
-      date: '01/02/2020',
-      campaignName: 'Stop Climate Change',
-      influencer: 'Elijah Woods x Jamie Fine',
-      status1: 1,
-      sponsor: 'Open',
-      status2: 1,
-    },
-    {
-      date: '01/02/2020',
-      campaignName: 'Stop Climate Change',
-      influencer: 'Elijah Woods x Jamie Fine',
-      status1: 1,
-      sponsor: 'Open',
-      status2: 1,
-    },
-    {
-      date: '01/02/2020',
-      campaignName: 'Stop Climate Change',
-      influencer: 'Elijah Woods x Jamie Fine',
-      status1: 1,
-      sponsor: 'Open',
-      status2: 1,
-    },
-    {
-      date: '01/02/2020',
-      campaignName: 'Stop Climate Change',
-      influencer: 'Elijah Woods x Jamie Fine',
-      status1: 2,
-      sponsor: 'Open',
-      status2: 1,
-    },
-    {
-      date: '01/02/2020',
-      campaignName: 'Stop Climate Change',
-      influencer: 'Elijah Woods x Jamie Fine',
-      status1: 1,
-      sponsor: 'Open',
-      status2: 3,
-    }
-  ];
   ngOnInit(): void {
   }
 }
