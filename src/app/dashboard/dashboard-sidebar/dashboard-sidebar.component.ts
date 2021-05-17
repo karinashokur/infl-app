a0543db65769556717a9ce96b0a19ed0cd282ec4
Karina <karina.shokur@devit-team.com>
2021-05-17 00:16:28 +0000

    fixed sidebar crash

--- src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -6,6 +6,9 @@ import {LogoutEventEmitterService} from './logout-modal/logout-event-emitter.ser
 import {user} from '../../constants';
 import {DashboardSidebarService} from './dashboard-sidebar.service';
 import {DashboardService} from '../dashboard.service';
+import {GETUSERDETAILSSIDEBAR} from "../../Apollo/queries";
+import {HttpHeaders} from "@angular/common/http";
+import {Apollo} from "apollo-angular";
 export class UserSideBar {
   userId;
   imgSrc;
@@ -47,18 +50,58 @@ export class DashboardSidebarComponent implements OnInit {
               private sessionStorageService: SessionStorageService,
               private logoutEventEmitterService: LogoutEventEmitterService,
               private dashboardSidebarService: DashboardSidebarService,
-              private dashboardService: DashboardService) {
+              private dashboardService: DashboardService,
+              private apollo: Apollo) {
+    if (!this.localstorageService.isLoggedIn()) {
+      this.router.navigate(['login']);
+    }
   }
   showLogoutPopup: boolean;
   user: UserSideBar;
   ngOnInit(): void {
-    if (!this.localstorageService.isLoggedIn()) {
-      this.router.navigate(['login']);
-    } else {
-      this.dashboardSidebarService.getUser().then((data: UserSideBar) => {
+      this.apollo.mutate({
+        mutation: GETUSERDETAILSSIDEBAR,
+        variables: {
+          id: this.localstorageService.getId()
+        },
+        context: {
+          headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.localstorageService.getJwtToken()),
+        }
+      }).toPromise().then((data: UserQueryDetail) => {
+        this.user = this.convertUserQueryDetailToUserSideBar(data);
+        console.log(this.convertUserQueryDetailToUserSideBar(data));
         console.log(data);
-        this.user = data;
       });
+    console.log('ng on inti finish fetched');
+  }
+  convertUserQueryDetailToUserSideBar(data) {
+    if (this.localstorageService.getUserType() === '1') {
+      return {
+        userId: data.data.user.id,
+        imgSrc: data.data.user.influencer.googlePhotoUrl,
+        firstName: data.data.user.influencer.firstName,
+        lastName: data.data.user.influencer.lastName,
+        position: 'Influencer(s)',
+        companyName: 'YouTube'
+      };
+    } else if (this.localstorageService.getUserType() === '2') {
+      return {
+        userId: '',
+        imgSrc: 'assets/images/Shelli.png',
+        firstName: data.data.user.non_profit.firstName,
+        lastName: data.data.user.non_profit.lastName,
+        position: 'Placeholder',
+        companyName: data.data.user.non_profit.organisation,
+      };
+    } else if (this.localstorageService.getUserType() === '3') {
+      return {
+        userId: '',
+        imgSrc: 'assets/images/Romeo.png',
+        firstName: data.data.user.sponsor.firstName,
+        lastName: data.data.user.sponsor.lastName,
+        position: 'Representative',
+        companyName: data.data.user.sponsor.organisation,
+      };
     }
   }
   showLogout() {
